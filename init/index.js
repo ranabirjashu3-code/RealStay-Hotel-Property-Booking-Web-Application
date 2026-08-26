2e398fd11637f5f400fae46d2e564fa9321c25df
ranabirjashu3-code <ranabirjashu3@gmail.com>
2026-08-27 03:12:35 +0530

    ready for deployment

--- init/index.js
+++ init/index.js
@@ -1,33 +1,35 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
 const initData = require("./data.js");
 const Listing = require("../models/listing.js");
-const { init } = require("../models/review.js");
 
 const MONGO_URL = process.env.ATLAS_URL;
 
-main()
-  .then(() => {
-    console.log("Connected to DB");
-  })
-  .catch((err) => {
-    console.log(err);
-  });
-
 async function main() {
   await mongoose.connect(MONGO_URL);
+  console.log("Connected to DB");
+
+  await initDB();
+
+  await mongoose.connection.close();
+  console.log("Database connection closed");
 }
 
 const initDB = async () => {
-    await Listing.deleteMany({});
-    initData.data = initData.data.map((obj) =>({
-      ...obj,
-      owner: "6a357e381727c8d8ccfe277c"
-    }));
-    await Listing.insertMany(initData.data);
+  await Listing.deleteMany({});
 
-    console.log("Data was initialized");
-  
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "6a357e381727c8d8ccfe277c"
+  }));
+
+  await Listing.insertMany(initData.data);
+
+  console.log("Data was initialized");
 };
 
-initDB();
+main().catch((err) => {
+  console.log(err);
+});
